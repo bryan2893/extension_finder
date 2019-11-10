6a4791d7eaa2c8a981754fbb4410ed3d5c0dd43c
Bryan Hernandez <hernandezbryan2893gmail.com>
2019-11-10 14:27:09 -0600

    Ports number extension is ready. It Works good.

--- content.js
+++ content.js
@@ -1,7 +1,17 @@
 //File with js code for execution in current page.
 console.log("Content.js is loaded!");
+
 chrome.runtime.onMessage.addListener(messageReceiver);
 
 function messageReceiver(request){
-    console.log(request);
+    switch(request.type){
+        case "error":
+            console.log(request.payload);
+            break;
+        case "algo":
+            break;
+        case "imprimir":
+            console.log("contenido: "+ request.payload);
+            break;
+    }
 }
